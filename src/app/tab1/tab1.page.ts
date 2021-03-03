ae532ba447f476e2262b78c5b7c4e62f5eee4fd5
Ayush Vipul <ayush.vipul@razorpay.com>
2021-03-03 17:53:25 +0530

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
 
+declare var RazorpayCheckout:any;
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
   constructor() {}
 
+
+  payWithRazorpay() {
+    var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: "INR", // your 3 letter currency code
+      key: "rzp_live_gM2B9AtiieU4AC", // your Key Id from Razorpay dashboard
+      amount: 100, // Payment amount in smallest denomiation e.g. cents for USD
+      name: 'Razorpay',
+      prefill: {
+        email: 'test@razorpay.com',
+        contact: '9990009991',
+        name: 'Razorpay'
+      },
+      theme: {
+        color: '#F37254'
+      },
+      modal: {
+        ondismiss: function () {
+          alert('dismissed')
+        }
+      }
+    };
+
+    var successCallback = function (payment_id) {
+      alert('payment_id: ' + payment_id);
+    };
+
+    var cancelCallback = function (error) {
+      alert(error.description + ' (Error ' + error.code + ')');
+    };
+
+    RazorpayCheckout.open(options, successCallback, cancelCallback);
+  }
 }
